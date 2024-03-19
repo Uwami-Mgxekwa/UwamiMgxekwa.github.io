1d53d2ee17966409a7df3ced72fc16d6d0a2ba62
Uwami Mgxekwa <112516594+Uwami-Mgxekwa@users.noreply.github.com>
2024-03-19 15:22:21 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /*                  typing animation         */
 var typed = new Typed(".typing", {
-    strings:["","Web Designer", "Web Developer", "Graphic Designer"],
+    strings:["","Java Developer", "SQL Developer", "Project Manager"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
